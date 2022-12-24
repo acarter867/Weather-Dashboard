c0209b5ceea3752c7177e9350a5a00d57a927c99
ACarter867 <acarter867@icloud.com>
2022-12-24 02:56:11 -0500

    added 5 day forecast cards. only date is functional

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -11,12 +11,9 @@ $(function(){
     btnSearch.on('mouseup', () => {
         btnSearch.css("background-color", "rgb(100, 165, 229)");
     })
-
     //search city
     btnSearch.on('click', displaySearch)   
 });
-
-
 
 function displaySearch(){
     let today = new Date();
@@ -26,15 +23,17 @@ function displaySearch(){
     today = '(' + mm + '/' + dd + '/' + yyyy + ')';
     let city = searchCity();
     
-    let queryCity = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=' + APIKey;
+    let queryCity = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + APIKey;
     
+    //call api for coordinates using city name
     fetch(queryCity)
     .then(result => {
         console.log(result.status);
         return result.json()
     })
     .then(data => {
-        let queryCoord = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&cnt=40&appid=' + APIKey;
+        //pass in coordinates for city and pull weather data
+        let queryCoord = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&appid=' + APIKey;
         fetch(queryCoord)
         .then(result => {
             console.log("Status: ", result.status)
@@ -43,17 +42,18 @@ function displaySearch(){
         .then(data => {
             console.log(data)
             updateCurrent(data, today)
+            getDays(data);
         });
         let searchedCities = localStorage.getItem('history');
         let parsedSearch = JSON.parse(searchedCities);
         if(parsedSearch == null || !parsedSearch.includes(city)){
             addToHistory(autoCaps(city));
-        }      
+        }
         $('.txt-search').val('');
     });
-
 }
 
+//display weather data of current city
 function updateCurrent(data, date){
     $('.city-name').text(data.city.name + " " + date);
     $('.current-temp').text("Temp: " + data.list[0].main.temp + " °F");
@@ -61,6 +61,8 @@ function updateCurrent(data, date){
     $('.current-humidity').text("Humidity: " + data.list[0].main.humidity + "%");
 }
 
+//add current search to history and push to local storage. 
+//would likely be faster to push all previously searched weather data to local storage.
 function addToHistory(cName){
     let entries = [];
     entries = localStorage.getItem('history');
@@ -73,7 +75,6 @@ function addToHistory(cName){
         parsedEntry.push(cName);
         localStorage.setItem('history', JSON.stringify(parsedEntry));
     }
-    
     let newBtn = $('<button></button>').text(cName);
     newBtn.on('click', () => {
         $('.txt-search').val(cName);
@@ -82,6 +83,7 @@ function addToHistory(cName){
     $('.history').append(newBtn);
 }
 
+//pull previous searches from localstorage & display as buttons
 function getSearchHistory(){
     let previousEntries = localStorage.getItem('history');
     let parsedEntries = JSON.parse(previousEntries);
@@ -109,6 +111,70 @@ function autoCaps(s){
     return resultString;
 }
 
+//set daily card info
+function dailyForecast(obj){
+    let day = $('<div></div>');
+    day.attr('class', 'card');
+
+    let dailyDate = $('<h5></h5>').text(obj.date);
+    day.append(dailyDate);
+
+    let dailyIcon = $('<h6></h6>').text(obj.icon);
+    day.append(dailyIcon);
+
+    let dailyTemp = $('<h6></h6>').text(obj.temp);
+    day.append(dailyTemp);
+
+    let dailyWind = $('<h6></h6>').text(obj.wind);
+    day.append(dailyWind);
+
+    let dailyHumidity = $('<h6></h6>').text(obj.humidity);
+    day.append(dailyHumidity);
+
+    $('.infographic').append(day);
+}
+
+//sort by day & pull highest forcasted temp for card display
+function getDays(data){
+    $('.infographic').empty();
+
+    let day = new Date();
+    let yyyy = day.getFullYear();
+    let mm = String(day.getMonth() + 1).padStart(2, '0');
+    let dd = String(day.getDate()).padStart(2, '0');
+    let today = yyyy + "-" + mm + "-" + dd;
+    console.log(today);
+
+    let futureDates = [];
+    //get list of *next* 5 dates 
+    for(let i = 0; i < data.list.length; i++){
+        let dayTime = data.list[i].dt_txt.split(" ");
+        let day = dayTime[0];
+        console.log(day)
+        if(day > today && !futureDates.includes(day)){
+            let formatDates = day.split('-');
+            futureDates.push(day);
+        }
+    }
+    console.log(futureDates.length)
+
+    for(let i = 0; i < futureDates.length; i++){
+        reformatFutureDate = futureDates[i].split("-");
+        reformattedDate = reformatFutureDate[1] + "/" + reformatFutureDate[2] + "/" + reformatFutureDate[0];
+        const dayOBJ = {
+            date: reformattedDate,
+            icon: "123",
+            temp: "Temp: ",
+            wind: "Wind: ",
+            humidity: "Humidity: "
+        }
+        dailyForecast(dayOBJ)
+    }
+
+    console.log(futureDates)
+}
+
+//pull temp from input field and send coordinates to api call
 function searchCity(){
     let input = $('.txt-search');
     let cityName = input.val();
